317004f2b1b4bcbdd8c152be3a7c1eee6d168b9b
EliasOPrado <eliaspradoprofessional@outlook.com>
2022-06-21 18:00:42 -0300

    chore:initial implementation of cache.

--- third_party_api/settings.py
+++ third_party_api/settings.py
@@ -75,6 +75,18 @@ TEMPLATES = [
     },
 ]
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "example"
+    }
+}
+
+
 WSGI_APPLICATION = 'third_party_api.wsgi.application'
 
 
