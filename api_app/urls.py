ab495d8907c26d1aef290afb8ebbbdacac73a6bd
EliasOPrado <eliaspradoprofessional@outlook.com>
2022-06-23 00:21:12 -0300

    chore: added app_name to easily call urls into testing.

--- api_app/urls.py
+++ api_app/urls.py
@@ -3,7 +3,9 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 from . import views
 
+app_name = 'profiles_api'
+
 urlpatterns = [
-    path("", views.ListProfiles.as_view()),
+    path("", views.ListProfiles.as_view(), name='api'),
     path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
 ]
