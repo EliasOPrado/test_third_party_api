71658f1811a9b0d5e2c3dc33a17b5b5293d649ba
EliasOPrado <eliaspradoprofessional@outlook.com>
2022-06-23 10:00:30 -0300

    test: created unit tests, checking different part of logic.

--- api_app/tests/test_api.py
+++ api_app/tests/test_api.py
@@ -1,52 +1,42 @@
-from rest_framework.test import APITestCase
-from rest_framework.test import APIClient
+from django.contrib.auth.models import User
+from django.urls import reverse
 from rest_framework import status
 from rest_framework.authtoken.models import Token
-
-from django.urls import reverse
-from django.contrib.auth.models import User
+from rest_framework.test import APIClient, APITestCase
 
 
 class TestUsersAPI(APITestCase):
-    
     def setUp(self):
         self.client = APIClient()
         user = User(
-            username="admin", 
-            password="@adr153", 
-            is_active=True, 
+            username="admin",
+            password="@adr153",
+            is_active=True,
             is_superuser=True,
-            is_staff=True)
+            is_staff=True,
+        )
         user.save()
 
         self.token = Token.objects.create(user=user)
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
 
-        self.response = self.client.get(
-            reverse('profiles_api:api'),
-            format='json'
-            )
+        self.response = self.client.get(reverse("profiles_api:api"), format="json")
 
     def test_profile_endpoint(self):
         # test if endpoint is returning what is needed.
         self.assertEqual(self.response.status_code, status.HTTP_200_OK)
-    
+
     def test_if_initial_endpoint_page_is_none(self):
-        self.assertEqual(self.response.data['previousPage'], None)
+        self.assertEqual(self.response.data["previousPage"], None)
 
     def test_if_users_is_a_list(self):
-        self.assertEqual(type(self.response.data['users']), list)
+        self.assertEqual(type(self.response.data["users"]), list)
 
     def test_if_endpont_has_page_number(self):
-        self.assertTrue(self.response.data['pageNumber'])
+        self.assertTrue(self.response.data["pageNumber"])
 
     def test_if_endpont_has_page_size(self):
-        self.assertTrue(self.response.data['pageSize'])
+        self.assertTrue(self.response.data["pageSize"])
 
     def test_if_endpont_has_total_count(self):
-        self.assertTrue(self.response.data['totalCount'])
-
-
-
-       
-        
+        self.assertTrue(self.response.data["totalCount"])
