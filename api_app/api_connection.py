0b99d46b77d9f0492e4103191d5101c9fda0e02c
EliasOPrado <eliaspradoprofessional@outlook.com>
2022-06-23 10:01:20 -0300

    refactor: removed users.json connection.

--- api_app/api_connection.py
+++ api_app/api_connection.py
@@ -1,11 +1,6 @@
-import json
-
 import requests
 
-# Read file
-with open("users.json", "r", encoding="utf-8") as data:
-    data = json.load(data)
 
-# url = "https://storage.googleapis.com/juntossomosmais-code-challenge/input-backend.json"
-# response = requests.get(url)
-# data = response.json()
+url = "https://storage.googleapis.com/juntossomosmais-code-challenge/input-backend.json"
+response = requests.get(url)
+data = response.json()
